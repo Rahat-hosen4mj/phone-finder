28519436de7ed21206795d88335249e7b538ab56
Your Name <you@example.com>
2022-04-20 19:52:19 +0600

    ui check done

--- custom.js
+++ custom.js
@@ -9,8 +9,13 @@ const searchPhone = () => {
 }
 
 const displayPhones = phones => {
+    // remove previous display phones
     const searchResult = document.getElementById('display-phone');
     searchResult.textContent = '';
+
+    // remove previous single phone details
+    const phoneDetails = document.getElementById('phone-details');
+    phoneDetails.textContent = '';
 
     // check condition, phone found or not
     const phoneLength = phones.length;
@@ -22,13 +27,14 @@ const displayPhones = phones => {
         `;
         searchResult.appendChild(div);
     }
-    const twentyPhones = phones.slice(0,20);
     
+    // 20 tar besi phone ekstahe UI teh dekhabo na
+    const twentyPhones = phones.slice(0, 20);
     twentyPhones.forEach(phone => {
         console.log(twentyPhones.length)
-            const div = document.createElement('div');
-            div.classList.add('col');
-            div.innerHTML = `
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
         <div class="card h-100">
              <img src="${phone.image}" class="card-img-top mx-auto p-3  w-75" alt="...">
             <div class="card-body">
@@ -38,10 +44,10 @@ const displayPhones = phones => {
             </div> 
         </div>
         `;
-            searchResult.appendChild(div);
-        }
+        searchResult.appendChild(div);
+    }
     )
-    
+
 
 }
 
@@ -61,28 +67,39 @@ const displayPhoneDetails = name => {
     div.innerHTML = `
         <div class="card h-100">
              <img src="${name.image}" class="card-img-top mx-auto p-3  img-fluid" alt="...">
-            <div class="card-body">
-            <h5 class="card-text text-primary">Brand: ${name.brand}</h5>
+             <div class="card-body">
+                <h5 class="card-text text-primary">Brand: ${name.brand}</h5>
                  <h6 class="card-title text-success">Name : ${name.name}</h6>
                  <h6 class="card-text text-secondary">${name.releaseDate ? name.releaseDate : 'Release Date : Not Found'}</h6>
                  <p><b>Chipset</b>: ${name.mainFeatures.chipSet}</p>
                  <p><b>Display size</b>: ${name.mainFeatures.displaySize}</p>
                  <p><b>Memory</b> : ${name.mainFeatures.chipSet}</p>
                  <p><b>Storage</b>: ${name.mainFeatures.storage}</p>
-                 <p><b>Sensors</b> : <ol>
-                 <li>${name.mainFeatures.sensors[0]}</li>
-                 <li>${name.mainFeatures.sensors[1]}</li>
-                 <li>${name.mainFeatures.sensors[2]}</li>
-                </ol>  </p> 
-                <!------- others information --------->
-                <p><b>Others</b> : <ul>
-                <li> Bluetooth: ${name.others.Bluetooth}</li>
-                <li>USB: ${name.others.USB}</li>
-               </ul>  </p>   
+                  <div class="container px-4">
+                        <div class="row gx-5">
+                            <div class="col">
+                                <div class="p-3 border bg-light">
+                                    <ol>
+                                    <b>Sensors information</b>
+                                    <li>${name.mainFeatures.sensors[0]}</li>
+                                    <li>${name.mainFeatures.sensors[1]}</li>
+                                    <li>${name.mainFeatures.sensors[2]}</li>
+                                    </ol>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="p-3 border bg-light">
+                                <ul>
+                                <b>Others information</b>
+                                <li> Bluetooth: ${name.others.Bluetooth}</li>
+                                <li>USB: ${name.others.USB}</li>
+                                </ul>
+                                </div>
+                            </div>
+                        </div>
+                   </div>      
             </div>
-            
         </div>
         `;
     phoneDetails.appendChild(div);
-
 }
